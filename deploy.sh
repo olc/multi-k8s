64e4ab9fa4ced19aa046325e38560b10cf0c66fd
Olivier Le Cam <olc@glou.fr>
2019-08-14 15:51:31 +0200

    fix the docker hub user to push image to

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t olc/multi-client:latest -t olc/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t olc/multi-server:latest -t olc/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t olc/multi-worker:latest -t olc/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t olecam/multi-client:latest -t olecam/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t olecam/multi-server:latest -t olecam/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t olecam/multi-worker:latest -t olecam/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push olc/multi-client:latest
-docker push olc/multi-server:latest
-docker push olc/multi-worker:latest
+docker push olecam/multi-client:latest
+docker push olecam/multi-server:latest
+docker push olecam/multi-worker:latest
 
-docker push olc/multi-client:$SHA
-docker push olc/multi-server:$SHA
-docker push olc/multi-worker:$SHA
+docker push olecam/multi-client:$SHA
+docker push olecam/multi-server:$SHA
+docker push olecam/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=olc/multi-server:$SHA
-kubectl set image deployments/client-deployment client=olc/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=olc/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=olecam/multi-server:$SHA
+kubectl set image deployments/client-deployment client=olecam/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=olecam/multi-worker:$SHA
